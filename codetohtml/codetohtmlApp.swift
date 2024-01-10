530c5505493e104d0574f1410972ff3f59ee5a60
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2024-01-10 19:45:23 +0100

    Initial Commit

---
+++ codetohtml/codetohtmlApp.swift
@@ -0,0 +1,17 @@
+//
+//  codetohtmlApp.swift
+//  codetohtml
+//
+//  Created by Jose Manuel Ortiz Sanchez on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct codetohtmlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
